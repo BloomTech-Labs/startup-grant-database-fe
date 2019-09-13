2d47e86ff2f5f8f45e530ef6fb73c8dfaa5e0962
Rory Murray <rory.christian.murray@gmail.com>
2019-09-13 15:40:50 -0400

    Create login types

--- grantly/src/actions/types.js
+++ grantly/src/actions/types.js
@@ -1,3 +1,6 @@
 export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_ERROR = "FETCH_ERROR";
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_ERROR = "LOGIN_ERROR";
