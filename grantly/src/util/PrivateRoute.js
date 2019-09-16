82b20529419f5444f21bb862286ef26b0d2178f7
Michael Harris <mhguitar1997@gmail.com>
2019-09-16 10:34:01 -0700

    If user is authenticated go to grants or go to login component

--- grantly/src/util/PrivateRoute.js
+++ grantly/src/util/PrivateRoute.js
@@ -2,7 +2,10 @@ import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
 const PrivateRoute = ({component: Component, ...rest}) => {
-    return  
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    return  isAuthenticated ? <Route {...rest} render={props => <Component {...props} /> } /> : <Redirect to="/grants" /> ;
+
     //if user is authenticated render the component
     // otherwise redirect them
 }
