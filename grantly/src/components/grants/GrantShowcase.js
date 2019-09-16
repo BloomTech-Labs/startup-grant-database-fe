ae2bcf54910738273c17d4c9c693160d381287d0
Rory Murray <rory.christian.murray@gmail.com>
2019-09-16 19:26:23 -0400

    Create GrantShowcase component

--- grantly/src/components/grants/GrantShowcase.js
+++ grantly/src/components/grants/GrantShowcase.js
@@ -1,15 +1,14 @@
 // Dependencies
 import React from "react";
-import { connect } from "react-redux";
 
 // Objects
 
 // Styles
 
-const Grant = props => {
-  // console.log("Grant props", props);
+const GrantShowcase = props => {
+  console.log("GrantShowcase props", props);
   return (
-    <div className="grant-card">
+    <div className="grant-showcase">
       <h2>{props.grant.competition_name}</h2>
       <div>{props.grant.amount}</div>
       <div>{props.grant.area_focus}</div>
@@ -17,4 +16,4 @@ const Grant = props => {
   );
 };
 
-export default Grant;
+export default GrantShowcase;
