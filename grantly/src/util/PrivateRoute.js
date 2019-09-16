1a465953ce74c5921c8caf1d2ea4a8432f2d927f
Michael Harris <mhguitar1997@gmail.com>
2019-09-16 10:12:58 -0700

    Structure in place for private route HOC

--- grantly/src/util/PrivateRoute.js
+++ grantly/src/util/PrivateRoute.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    return  
+    //if user is authenticated render the component
+    // otherwise redirect them
+}
+
+export default PrivateRoute;
