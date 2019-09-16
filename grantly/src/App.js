666c05df59747725eac24e9d678bf2239135bd1b
Michael Harris <mhguitar1997@gmail.com>
2019-09-16 13:48:45 -0700

    Basic structure for filters without material styles

--- grantly/src/App.js
+++ grantly/src/App.js
@@ -1,30 +1,25 @@
 // Dependencies
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom"
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Objects
 import GrantList from "./components/GrantList";
 import NavBar from "./components/Navbar";
 
-
 // Stylings
 import "./App.css";
+import Filters from "./components/Filters";
 
 function App() {
-
-
   return (
     <Router>
-
-  
-    <div className="App">
-      <div>Welcome to Grantly</div>
-      <Route path="/" component={NavBar} />
-      <Route path="/grants" component={GrantList} />
-
-    </div>
-</Router>
+      <div className="App">
+        <div>Welcome to Grantly</div>
+        {/* <Route path="/" component={NavBar} />
+        <Route path="/grants" component={GrantList} /> */}
+        <Filters />
+      </div>
+    </Router>
   );
 }
 
