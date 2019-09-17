abc31e93acb654592ef837c5d6d66ab5235c49dd
Rory Murray <rory.christian.murray@gmail.com>
2019-09-16 20:43:10 -0400

    Create selectGrant action

--- grantly/src/actions/types.js
+++ grantly/src/actions/types.js
@@ -7,5 +7,7 @@ export const LOGIN_ERROR = "LOGIN_ERROR";
 export const CREATE_START = "CREATE_START";
 export const CREATE_SUCCESS = "CREATE_SUCCESS";
 export const CREATE_ERROR = "CREATE_ERROR";
+export const SELECT_GRANT = "SELECT_GRANT";
+
 //Filter Type
 export const FILTER_GRANTS = "FILTER_GRANTS";
