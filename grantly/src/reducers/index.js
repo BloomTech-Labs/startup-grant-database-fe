80dbe2d3efc9e35e7aa9269851cdac29ea4322d4
Michael Harris <mhguitar1997@gmail.com>
2019-09-17 23:49:39 -0700

    Merged with master and got an ugly version of filters working without reset

--- grantly/src/reducers/index.js
+++ grantly/src/reducers/index.js
@@ -49,48 +49,48 @@ export const rooterReducer = (state = initialState, { type, payload }) => {
       };
     case FILTER_GRANTS:
       let newList = [];
+      // Don't pay attention to this mess :)
+      state.data.filter(grant => {
+        Object.entries(payload).map(filter => {
+          filter[1].map(userFilters => {
+            if(filter[0] === "amount"){
+              console.log("newList", newList)
+              console.log("wazzup", userFilters.replace(/[^0-9\-]/g, ''));
+              console.log("zup", userFilters.replace(/[^0-9\+]/g, ''));
 
-     
+              if(userFilters.includes("-")){
+                const min = userFilters.split("-")[0].replace(/\D/g,'')
+                const max = userFilters.split("-")[1].replace(/\D/g,'')
+                if(grant[filter[0]] >= min && grant[filter[0]] <= max){
+                  newList.push(grant)
+                }
+                
+              } else if(grant[filter[0]] <= userFilters.replace(/\D/g,'')){
+                newList.push(grant);
+              } else if (userFilters.replace(/[^0-9\+]/g, '').includes("+")){
+                if(grant[filter[0]] >= userFilters.replace(/\D/g,'')){
+                  console.log("no", userFilters.replace(/\D/g,''))
+                  newList.push(grant)
+                }
+              }
+            } else {
+              if (
+                grant[filter[0]].toLowerCase().includes(userFilters.toLowerCase())
+              ) {
+                newList.push(grant);
+                console.log("newList", newList);
+              }
+
+            }
+          });
+          // console.log("filter", filter)
+        });
+      });
 
       return {
         ...state,
         filters: payload,
-        filteredGrants:  [...state.data.filter(grant => {
-          Object.entries(payload).map(filter => {
-            filter[1].map(userFilters => {
-              if(filter[0] === "amount"){
-                console.log("newList", newList)
-                console.log("wazzup", userFilters.replace(/[^0-9\-]/g, ''));
-                console.log("zup", userFilters.replace(/[^0-9\+]/g, ''));
-  
-                if(userFilters.includes("-")){
-                  const min = userFilters.split("-")[0].replace(/\D/g,'')
-                  const max = userFilters.split("-")[1].replace(/\D/g,'')
-                  if(grant[filter[0]] >= min && grant[filter[0]] <= max){
-                    newList.push(grant)
-                  }
-                  
-                } else if(grant[filter[0]] <= userFilters.replace(/\D/g,'')){
-                  newList.push(grant);
-                } else if (userFilters.replace(/[^0-9\+]/g, '').includes("+")){
-                  if(grant[filter[0]] >= userFilters.replace(/\D/g,'')){
-                    console.log("no", userFilters.replace(/\D/g,''))
-                    newList.push(grant)
-                  }
-                }
-              } else {
-                if (
-                  grant[filter[0]].toLowerCase().includes(userFilters.toLowerCase())
-                ) {
-                  newList.push(grant);
-                  console.log("newList", newList);
-                }
-  
-              }
-            });
-            // console.log("filter", filter)
-          });
-        })]
+        filteredGrants: newList
 
         // [...state.data.filter(grant => {
         //   return grant.amount <= 1000;
