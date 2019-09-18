0b8be39d5cd64abf922456d888e27616d69650a8
Rory Murray <rory.christian.murray@gmail.com>
2019-09-18 11:09:45 -0400

    Grant styles

--- grantly/src/components/grants/Grant.js
+++ grantly/src/components/grants/Grant.js
@@ -6,26 +6,40 @@ import { connect } from "react-redux";
 import { selectGrant } from "../../actions";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import BookmarkIcon from "@material-ui/icons/Bookmark";
 
 // Styles
+import "../../App.scss";
 
 const Grant = props => {
-  // console.log("Grant props", props);
+  console.log("Grant props", props);
 
   const selectGrant = () => {
     props.selectGrant(props.grant);
   };
   return (
-    <div className="grant-card">
-      <h2>{props.grant.competition_name}</h2>
-      <div>{props.grant.amount}</div>
-      <div>{props.grant.area_focus}</div>
+    <Container className="grant-card">
+      <Grid container justify="flex-end">
+        <Grid item>
+          <BookmarkIcon className="bookmark" />
+        </Grid>
+      </Grid>
+      <h3>{props.grant.competition_name}</h3>
+      <Grid container direction="column">
+        <div item>Deadline</div>
+        <div item>{props.grant.most_recent_application_due_date}</div>
+      </Grid>
+      <Grid container direction="column">
+        <div item>Amount</div>
+        <div item>${props.grant.amount}</div>
+      </Grid>
       <Container maxWidth="sm">
         <Button variant="contained" color="default" onClick={selectGrant}>
           Select
         </Button>
       </Container>
-    </div>
+    </Container>
   );
 };
 
