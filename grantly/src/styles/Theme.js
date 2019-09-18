1f1d569a583cc4c0be85bbeb38cea99b9cc3672b
Michael Harris <mhguitar1997@gmail.com>
2019-09-18 13:58:29 -0700

    Add one material ui custom theme

--- grantly/src/styles/Theme.js
+++ grantly/src/styles/Theme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from "@material-ui/core/styles";
 export const theme = createMuiTheme({
     palette: {
         primary: {
-            main: "green"
+            main: "#3DB8B3"
         },
     }
 });
