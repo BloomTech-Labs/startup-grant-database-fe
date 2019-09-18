d496042bb83f9bfaf4eb0e537949b4bc3c650f0d
Rory Murray <rory.christian.murray@gmail.com>
2019-09-18 19:14:26 -0400

    Update SHowcase number formatting

--- grantly/src/components/grants/GrantShowcase.js
+++ grantly/src/components/grants/GrantShowcase.js
@@ -10,6 +10,9 @@ import Card from "@material-ui/core/Card";
 
 const GrantShowcase = props => {
   console.log("GrantShowcase props", props);
+  function formatNumbers(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
   const styles = grantStyles();
   if (props.isFetching) {
@@ -21,7 +24,12 @@ const GrantShowcase = props => {
   return (
     <Card className={styles.showcase}>
       <h2>{props.grant.competition_name}</h2>
-      <div>{props.grant.amount}</div>
+      <div>
+        {" "}
+        {props.grant.amount
+          ? "$" + formatNumbers(props.grant.amount)
+          : "See website for details"}
+      </div>
       <div>{props.grant.area_focus}</div>
     </Card>
   );
