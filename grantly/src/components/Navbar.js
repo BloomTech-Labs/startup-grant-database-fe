d76adb0493d0d624514acf54ec87c5677b95ef48
Michael Harris <mhguitar1997@gmail.com>
2019-09-19 12:50:48 -0700

    Swipeable drawer added for mobile

--- grantly/src/components/Navbar.js
+++ grantly/src/components/Navbar.js
@@ -10,6 +10,14 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 import { textAlign } from "@material-ui/system";
 
 const useStyles = makeStyles(theme => ({
@@ -52,9 +60,39 @@ const NavBar = () => {
       return;
     }
 
-    setState(!open);
+    setIsOpen(!isOpen);
   };
   const classes = useStyles();
+
+  const sideList = side => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
   return (
     <AppBar className={classes.navbar} color="primary" position="sticky">
@@ -95,14 +133,29 @@ const NavBar = () => {
             Log out
           </Button>
         )} */}
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
+        <Media query="(max-width:800px)">
+          {matches =>
+            matches ? (
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+                onClick={toggleDrawer()}
+              >
+                <MenuIcon />
+              </IconButton>
+            ) : null
+          }
+        </Media>
+          <SwipeableDrawer
+            anchor="right"
+            open={isOpen}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer(true)}
+          >
+            {sideList("right")}
+          </SwipeableDrawer>
       </Toolbar>
     </AppBar>
   );
