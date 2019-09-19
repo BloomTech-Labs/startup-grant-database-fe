b3bfcc683d8f1292c8812afe8c909e837bb37092
Michael Harris <mhguitar1997@gmail.com>
2019-09-19 14:08:55 -0700

    Mobile tabs coming together

--- grantly/src/components/MobileTabs.js
+++ grantly/src/components/MobileTabs.js
@@ -1,61 +1,68 @@
-import React, {useState} from 'react';
-import SwipeableViews from 'react-swipeable-views';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import React, { useState } from "react";
+import SwipeableViews from "react-swipeable-views";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const MobileTabs = () => {
-    const [tab, setTab] = useState(0)
-    const tabs = ["Home", "Bookmarked", "Account"]
-    const value = 0;
-    const index = 0;
-    const TabPanel = () => {
-      return (
-        <Typography
-          component="div"
-          role="tabpanel"
-          hidden={value !== index}
-          id={`full-width-tabpanel-${index}`}
-          aria-labelledby={`full-width-tab-${index}`}
-        >
-          <Box p={3}>{tabs}</Box>
-        </Typography>
-      );
-    }
-    const changeTab = value => {
-        setTab(value);
-    }
-    
+  const [tab, setTab] = useState(0);
+  const tabs = ["Home", "Bookmarked", "Account"];
+  const value = 0;
+  const index = 0;
+  const TabPanel = () => {
     return (
-        <div>
-                    <Tabs
-          value={tab}
-          onChange={changeTab}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Item One"  />
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
- 
-                  <SwipeableViews>
-                  <TabPanel value={value} index={0}>
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`full-width-tabpanel-${index}`}
+        aria-labelledby={`full-width-tab-${index}`}
+      >
+        <Box p={3}>{tabs[tab]}</Box>
+      </Typography>
+    );
+  };
+  const changeTab = (event, value) => {
+    console.log("Change", value);
+    setTab(value);
+  };
+
+  const handleChangeIndex = index => {
+    setTab(index);
+  };
+
+  return (
+    <div>
+      <Tabs
+        value={tab}
+        onChange={changeTab}
+        indicatorColor="primary"
+        textColor="primary"
+        variant="fullWidth"
+        aria-label="full width tabs example"
+      >
+        {/* {tabs.map(tab => (
+          <Tab tabel={tab} key={tab} />
+        ))} */}
+        <Tab label={tabs[0]} />
+        <Tab label={tabs[1]} />
+        <Tab label={tabs[2]} />
+      </Tabs>
+
+      <SwipeableViews index={tab} onChangeIndex={handleChangeIndex}>
+        <TabPanel value={tab} index={0}>
           Item One
         </TabPanel>
-        <TabPanel value={value} index={1}>
+        <TabPanel value={tab} index={1}>
           Item Two
         </TabPanel>
-        <TabPanel value={value} index={2} >
+        <TabPanel value={tab} index={2}>
           Item Three
         </TabPanel>
-                  </SwipeableViews>
-            
-        </div>
-    );
+      </SwipeableViews>
+    </div>
+  );
 };
 
 export default MobileTabs;
