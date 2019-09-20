d2b234f6b1545a163c3b1602cc0ee5aecef354b3
Yusuf Nafey <yusufnafey@yahoo.com>
2019-09-20 12:10:36 -0700

    added drawer

--- grantly/src/components/grants/GrantList.js
+++ grantly/src/components/grants/GrantList.js
@@ -6,10 +6,60 @@ import { connect } from "react-redux";
 import Grant from "./Grant";
 import Loader from "react-loader-spinner";
 import { fetchApi } from "../../actions";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+const drawerWidth = 240;
 
 // Styles
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
 
 const GrantList = props => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(true);
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
+  }
+
   console.log("GrantList props", props);
   useEffect(() => {
     if (props.data.length === 0) {
@@ -22,8 +72,17 @@ const GrantList = props => {
     return <Loader type="Triangle" color="#00BFFF" height="100" width="100" />;
   }
   // console.log(user);
+
   return (
-    <div className="helloooooooooooo">
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
       {props.data.length > 0 ? (
         props.data.map(grant => {
           return <Grant grant={grant} key={grant.id} />;
@@ -31,7 +90,8 @@ const GrantList = props => {
       ) : (
         <div> Grants incoming! </div>
       )}
-    </div>
+      <div className={classes.drawerHeader}></div>
+    </Drawer>
   );
 };
 
