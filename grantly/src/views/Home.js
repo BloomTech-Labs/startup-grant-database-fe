70a84d0d506a13c92d6833cd029560e39a3e5999
Michael Harris <mhguitar1997@gmail.com>
2019-09-23 12:33:08 -0700

    Mobile filter box

--- grantly/src/views/Home.js
+++ grantly/src/views/Home.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GrantList from "../components/grants/GrantList";
 import Filters from "../components/Filters";
 import GrantShowcase from "../components/grants/GrantShowcase";
 import MobileTabs from "../components/MobileTabs";
-import SearchBar from "../components/SearchBar"
+import SearchBar from "../components/SearchBar";
 import Grid from "@material-ui/core/Grid";
 import Navbar from "../components/Navbar";
 import Media from "react-media";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import {  useTheme } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 
 const drawerWidth = 480;
 
@@ -58,6 +60,18 @@ const useStyles = makeStyles(theme => ({
 //   }
 // }));
 const Home = props => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = open => event => {
+    if (
+      event &&
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setIsOpen(!isOpen);
+  };
   const classes = useStyles();
 
   return (
@@ -67,7 +81,17 @@ const Home = props => {
       <Media query="(max-width:850px)">
         {matches =>
           matches ? (
-            <MobileTabs />
+            <>
+              <MobileTabs />
+              <SwipeableDrawer
+                anchor="bottom"
+                open={isOpen}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+              >
+              <Filters />
+              </SwipeableDrawer>
+            </>
           ) : (
             <div>
               <Grid container spacing={2} className="grid-container">
