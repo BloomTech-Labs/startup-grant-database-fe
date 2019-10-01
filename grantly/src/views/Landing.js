d54861bf0d01b697905941bb9a4b3c7f801b5b81
Michael Harris <mhguitar1997@gmail.com>
2019-10-01 14:45:28 -0700

    Move landing stlyes into styles folder

--- grantly/src/views/Landing.js
+++ grantly/src/views/Landing.js
@@ -2,71 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import Navbar from "../components/Navbar";
 import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
+import {landingStyles} from "../styles/landingStyles"
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Filter from "../components/Filters";
 import { fetchApi } from "../actions/index";
 import { flexbox } from "@material-ui/system";
 
-const useStyles = makeStyles(theme => ({
-  welcome: {
-    "& h1": {
-      fontSize: "3.5rem",
-      fontFamily: "adobe-garamond-pro",
-      fontWeight: 400,
-      margin: "1rem",
-      padding: "30px",
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "2rem",
-        padding: 0,
-        margin: 0
-      }
-    },
-    "& p": {
-      fontFamily: "adobe-garamond-pro",
-      fontSize: "32px",
-      margin: "5%",
-      [theme.breakpoints.down("xs")]: {
-        fontSize: "18px"
-      }
-    },
-    [theme.breakpoints.down("xs")]: {
-      margin: "1rem",
-      flexGrow: 2
-    }
-  },
-  grid: {
-    maxWidth: "100%",
-    height: "80%",
-    [theme.breakpoints.down("xs")]: {
-      marginTop: "3rem",
-      alignContent: "center",
-      display: "block"
-    }
-  },
-  container: {
-    height: "100vh"
-  },
-  link: {
-    textDecoration: "none",
-    "& button": {
-      fontFamily: "Roboto",
-      marginTop: "35px",
-      borderRadius: "2px",
-      [theme.breakpoints.down("sm")]: {
-        marginBottom: "30px"
-      }
-    }
-  },
-  button: {
-    width: "266px",
-    height: "58px"
-  }
-}));
+
 
 function Landing(props) {
-  const classes = useStyles();
+  const classes = landingStyles();
 
   return (
     <div className={classes.container}>
