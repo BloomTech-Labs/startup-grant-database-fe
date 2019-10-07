8047923dd4d14e9b9048126309917daf8d4f015d
Rory Murray <rory.christian.murray@gmail.com>
2019-10-07 15:30:19 -0400

    Create basic dialog

--- grantly/src/components/grants/dialogs/SuggestionDialog.js
+++ grantly/src/components/grants/dialogs/SuggestionDialog.js
@@ -1,28 +1,98 @@
 import React from "react";
 import { dialogStyles } from "../styles/DialogStyles";
-import Button from "@material-ui/core/Button";
+import {
+  Button,
+  Fab,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select
+} from "@material-ui/core";
 
 export default () => {
+  const [suggestion, setSuggestion] = React.useState("");
+  console.log("SuggestionDialog suggestion", suggestion);
+
   const [open, setOpen] = React.useState(false);
 
   const classes = dialogStyles();
 
   const handleOpen = () => {
     setOpen(true);
-    console.log("it's working!");
   };
 
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleChanges = name => ({ target: { value } }) => {
+    setSuggestion({ ...suggestion, [name]: value });
+  };
+
   return (
-    <Button
-      className={classes.applyButton}
-      variant="contained"
-      color="primary"
-      onClick={handleOpen}
-    >
-      Suggest Changes
-    </Button>
+    <>
+      <Button
+        className={classes.applyButton}
+        variant="contained"
+        color="primary"
+        onClick={handleOpen}
+      >
+        Suggest Changes
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Suggest changes</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please suggest changes in the below dialog
+          </DialogContentText>
+          <form>
+            <TextField
+              label="suggestion"
+              value={suggestion}
+              onChange={handleChanges("suggestion")}
+              margin="normal"
+              className={classes.formField}
+            />
+            <br />
+            {/* <FormControl>
+              <InputLabel htmlFor="muscles">Muscles</InputLabel>
+              <Select
+                value={muscles}
+                onChange={handleChanges("muscles")}
+                className={classes.formField}
+              >
+                {categories.map(category => {
+                  return (
+                    <MenuItem value={category} key={category}>
+                      {category}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+            <br />
+            <TextField
+              label="Description"
+              multiline
+              rows="4"
+              onChange={handleChanges("description")}
+              margin="normal"
+              className={classes.formField}
+            /> */}
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button color="primary" variant="outlined" onClick={handleSubmit}>
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
--- grantly/src/components/grants/styles/DialogStyles.js
+++ grantly/src/components/grants/styles/DialogStyles.js
@@ -3,5 +3,8 @@ import { makeStyles } from "@material-ui/core/styles";
 export const dialogStyles = makeStyles(theme => ({
   applyButton: {
     color: "#fff"
+  },
+  formField: {
+    width: 500
   }
 }));
