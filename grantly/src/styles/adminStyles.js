4854b2b7b6905ec66d0483ca8d6a7e250bcd8961
Michael Harris <mhguitar1997@gmail.com>
2019-10-08 11:34:23 -0700

    Add button to sho9w and hide filters for admin

---
+++ grantly/src/styles/adminStyles.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const adminStyles = makeStyles(theme => ({
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3)
+    },
+    gridContainer: {
+      flexWrap: "nowrap"
+    },
+    gridItem: {
+      padding: 30
+    },
+    root: {
+      display: "flex"
+    },
+    filters: {
+        transition: "all .3s ease-in-out",
+      },
+      hideFilters: {
+        transform: "translateX(100%)",
+      },
+      showFilters: {
+        transform: "translateX(0)",
+  
+      },
+    toolbar: theme.mixins.toolbar
+  }));
+  
