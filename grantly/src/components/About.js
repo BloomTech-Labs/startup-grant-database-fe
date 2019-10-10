906bc06218407a722f56c16a65f6fc1849d0eb95
Rory Murray <rory.christian.murray@gmail.com>
2019-10-10 18:24:14 -0400

    Add CSS-in-JS styling conventions and basic skeleton structure

--- grantly/src/components/About.js
+++ grantly/src/components/About.js
@@ -1,45 +1,47 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
+import { aboutStyles } from "../styles/aboutStyles";
 
 export default () => {
+  const classes = aboutStyles();
   return (
-    <Grid container style={{ border: "1px solid red" }}>
-      <Grid item style={{ border: "1px solid red" }}>
+    <Grid container justify="space-around" className={classes.aboutContainer}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Imani Russ
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Zabdiel Rosario
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Reed James
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Michael Harris
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Yusuf Nafey
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Philip Johnson
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Rory Murray
         </Typography>
       </Grid>
-      <Grid item style={{ border: "1px solid red" }}>
+      <Grid item className={classes.aboutCard}>
         <Typography variant="subtitle1" color="secondary">
           Chad Kidd
         </Typography>
---
+++ grantly/src/styles/aboutStyles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const aboutStyles = makeStyles(theme => ({
+  aboutContainer: {
+    border: "1px solid fuchsia"
+  },
+  aboutCard: {
+    border: "1px solid fuchsia",
+    margin: 10,
+    padding: 10
+  }
+}));
+
+export default aboutStyles;
