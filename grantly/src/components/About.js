7170ed26d084ff22515f90fc69e68111a9a53fdb
Rory Murray <rory.christian.murray@gmail.com>
2019-10-10 18:26:06 -0400

    Create top box

--- grantly/src/components/About.js
+++ grantly/src/components/About.js
@@ -5,47 +5,54 @@ import { aboutStyles } from "../styles/aboutStyles";
 export default () => {
   const classes = aboutStyles();
   return (
-    <Grid container justify="space-around" className={classes.aboutContainer}>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Imani Russ
-        </Typography>
+    <>
+      <Grid container justify="center">
+        <Grid item>
+          <Typography variant="body1">Some basic intro stuff here</Typography>
+        </Grid>
       </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Zabdiel Rosario
-        </Typography>
+      <Grid container justify="space-around" className={classes.aboutContainer}>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Imani Russ
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Zabdiel Rosario
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Reed James
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Michael Harris
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Yusuf Nafey
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Philip Johnson
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Rory Murray
+          </Typography>
+        </Grid>
+        <Grid item className={classes.aboutCard}>
+          <Typography variant="subtitle1" color="secondary">
+            Chad Kidd
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Reed James
-        </Typography>
-      </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Michael Harris
-        </Typography>
-      </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Yusuf Nafey
-        </Typography>
-      </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Philip Johnson
-        </Typography>
-      </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Rory Murray
-        </Typography>
-      </Grid>
-      <Grid item className={classes.aboutCard}>
-        <Typography variant="subtitle1" color="secondary">
-          Chad Kidd
-        </Typography>
-      </Grid>
-    </Grid>
+    </>
   );
 };
