9619c90501d569f9c1f62fcf1f276dfca39b3c62
Rory Murray <rory.christian.murray@gmail.com>
2019-10-10 19:00:42 -0400

    Set-up rough draft of Card structure

--- grantly/src/components/About.js
+++ grantly/src/components/About.js
@@ -1,32 +1,52 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Card } from "@material-ui/core";
 import { aboutStyles } from "../styles/aboutStyles";
 
 export default () => {
   const classes = aboutStyles();
   return (
-    <>
-      <Grid container justify="center" className={classes.aboutIntro}>
-        <Grid item>
-          <Typography variant="body1">
-            Founder Grants was built to solve one basic problem: connecting
-            early stage founders with appropriate available resources to fund
-            their inventions.
-          </Typography>
+    <Card className={classes.aboutCardContainer}>
+      <Card className={classes.aboutIntroCardContainer}>
+        <Grid container justify="center" className={classes.aboutIntro}>
+          <Grid item className={classes.aboutTopCard}>
+            <Typography variant="body1">
+              Founder Grants was built to solve one basic problem: connecting
+              early stage founders with appropriate available resources to fund
+              their inventions. It was developed by 1517 Fund in partnership
+              with Lambda School students.
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Grid container justify="space-around" className={classes.aboutContainer}>
-        <Grid item className={classes.aboutCard}>
-          <Typography variant="subtitle1" color="secondary">
-            Imani Russ
-          </Typography>
+
+        <Grid
+          container
+          justify="space-around"
+          className={classes.aboutContainer}
+        >
+          <Grid item xs={3}>
+            <Card className={classes.aboutCard}>
+              <Typography variant="subtitle1">
+                You can find out more about 1517 Fund here.
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card className={classes.aboutCard}>
+              <Typography variant="subtitle1">
+                You can find out more about Lambda School here.
+              </Typography>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card className={classes.aboutCard}>
+              <Typography variant="subtitle1">
+                You can find out more about the student team and the project,
+                including the open source code here.
+              </Typography>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item className={classes.aboutCard}>
-          <Typography variant="subtitle1" color="secondary">
-            Zabdiel Rosario
-          </Typography>
-        </Grid>
-      </Grid>
-    </>
+      </Card>
+    </Card>
   );
 };
--- grantly/src/styles/aboutStyles.js
+++ grantly/src/styles/aboutStyles.js
@@ -1,18 +1,29 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const aboutStyles = makeStyles(theme => ({
-  aboutContainer: {
-    border: "1px solid fuchsia"
+  aboutCardContainer: {
+    // border: "1px solid fuchsia"
+    margin: 1
+  },
+  aboutIntroCardContainer: {
+    // border: "1px solid fuchsia"
+    margin: 1
   },
   aboutCard: {
-    border: "1px solid fuchsia",
+    // border: "1px solid fuchsia",
     margin: 10,
     padding: 10
   },
   aboutIntro: {
-    border: "1px solid fuchsia",
+    // border: "1px solid fuchsia",
     marginTop: 15,
     marginBottom: 15
+  },
+  aboutTopCard: {
+    width: "75%",
+    // border: "1px solid turquoise",
+    margin: 20,
+    padding: 15
   }
 }));
 
