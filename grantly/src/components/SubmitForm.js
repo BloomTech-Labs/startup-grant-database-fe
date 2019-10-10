aca6903c71f44963e0ab5c1a6aeb13140f17efd3
Reed James <reedtjames1234@gmail.com>
2019-10-10 16:25:13 -0500

    Fields for reviewed and requests added, set to false

--- grantly/src/components/SubmitForm.js
+++ grantly/src/components/SubmitForm.js
@@ -28,16 +28,6 @@ import GrantDemo from "./submitForm/GrantDemo";
 import { typography } from "@material-ui/system";
 import moment from "moment";
 
-// const funding = [
-//   {
-//     value: true,
-//     label: "yes"
-//   },
-//   {
-//     value: false,
-//     label: "no"
-//   }
-// ];
 const AddGrant = props => {
   const steps = ["Grant Info", "Grant Focus", "Grant Demo"];
 
@@ -74,10 +64,9 @@ const AddGrant = props => {
     target_entrepreneur_demographic: "",
     notes: "",
     early_stage_funding: "",
-    // details_last_updated: Date.now()
-    // details_last_updated: new Date()
+    is_reviewed: false,
+    has_requests: false,
     details_last_updated: moment().format("YYYY-MM-DD")
-    // details_last_updated: moment().format("YYYY-MM-DD")
   });
 
   const handleChanges = event => {
@@ -172,21 +161,13 @@ const AddGrant = props => {
                       Back
                     </Button>
                   )}
-                  {/* <Fragment>
-                  {activeStep === steps.length - 1 ? (
-                    <Button>submit</Button>
-                  ) : (
-                    <Button> Next </Button>
-                  )}
-                  </Fragment> */}
+
                   <Button
                     variant="contained"
                     color="primary"
                     onClick={
                       activeStep === steps.length - 1 ? submitGrant : handleNext
                     }
-                    // onClick={handleNext}
-
                     className={styles.submit}
                   >
                     {activeStep === steps.length - 1 ? "submit" : "Next"}
@@ -198,283 +179,7 @@ const AddGrant = props => {
         </Paper>
       </main>
     </Fragment>
-
-    // <div classname={styles.grid}>
-    //   <Grid
-    //     // container
-    //     // classname={styles.grid}
-    //     direction="column"
-    //     justify="center"
-    //     alignItems="center"
-    //   >
-
-    //     <Grid className={styles.bottomBox}>
-    //       <div>
-    //         <form onSubmit={submitGrant}>
-    //           {/* <div className={styles.formContainer}>
-    //             <TextField
-    //               type="date"
-    //               className={styles.inputText}
-    //               name="details_last_updated"
-    //               value={grantInfo.details_last_updated}
-    //               onChange={handleChanges}
-    //               helperText="Details Last Updated"
-    //               margin="normal"
-    //               variant="outlined"
-    //             />
-    //           </div> */}
-    //           <div className={styles.divButton}>
-    //             <Button
-    //               type="submit"
-    //               variant="contained"
-    //               color="primary"
-    //               size="large"
-    //               className={styles.submit}
-    //             >
-    //               <span className={styles.label}>Submit</span>
-    //             </Button>
-    //           </div>
-    //         </form>
-    //       </div>
-    //     </Grid>
-    //   </Grid>
-    //   {/* </Container> */}
-    // </div>
   );
-  // return (
-  //   <div className={styles.root}>
-  //     {/* <Container fixed> */}
-  //     {/* <Media query="(max-width:850px)">
-  //       {matches => (matches ? null : <NavBar />)}
-  //     </Media> */}
-  //     {/* <h1>Submit a New Grant to Founder Grants</h1> */}
-  //     <Grid
-  //       // className={styles.grid}
-  //       container
-  //       direction="column"
-  //       justify="center"
-  //       alignItems="center"
-  //     >
-  //       {/* <Grid item className={styles.leftBox} sm={12} md={5}> */}
-  //       {/* <Grid item sm={12} md={4}> */}
-  //       {/* <Grid className={styles.topBox}> */}
-  //       <Grid>
-  //         {/* <div> */}
-  //         {/* <div className={styles.leftBox}> */}
-  //         <div className={styles.topBox}>
-  //           <h1>Submit a New Grant to Founder Grants</h1>
-  //           <p>
-  //             Please fill out all of the form fields on this page regarding the
-  //             grant you are submitting. If you are unsure of anything please
-  //             write “N/A” Thank you!
-  //           </p>
-  //         </div>
-  //       </Grid>
-  //       {/* <Grid item sm={12} md={8}> */}
-  //       {/* <Grid className={styles.bottomBox}> */}
-  //       <Grid>
-  //         <div className={styles.bottomBox}>
-  //           <form className={styles.form} onSubmit={submitGrant}>
-  //             <div className={styles.formContainer}>
-  //               <TextField
-  //                 // id="outlined-name"
-  //                 label="Competition Name"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="competition_name"
-  //                 placeholder="Competition Name"
-  //                 value={grantInfo.competition_name}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //               <TextField
-  //                 label="Type"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="type"
-  //                 placeholder="Type"
-  //                 value={grantInfo.type}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //               <TextField
-  //                 label="Area Focus"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="area_focus"
-  //                 placeholder="Area Focus"
-  //                 value={grantInfo.area_focus}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.formContainer}>
-  //               <TextField
-  //                 label="Sponsoring Entity"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="sponsoring_entity"
-  //                 placeholder="Sponsoring Entity"
-  //                 value={grantInfo.sponsoring_entity}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //               <TextField
-  //                 label="Website"
-  //                 type="url"
-  //                 className={styles.inputText}
-  //                 name="website"
-  //                 placeholder="Website"
-  //                 value={grantInfo.website}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-
-  //               <TextField
-  //                 label="Amount"
-  //                 type="number"
-  //                 className={styles.inputText}
-  //                 name="amount"
-  //                 placeholder="Amount"
-  //                 value={grantInfo.amount}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.formContainer}>
-  //               {/* <div> */}
-  //               <TextField
-  //                 label="Amount Notes"
-  //                 type="text"
-  //                 className={styles.notes}
-  //                 name="amount_notes"
-  //                 multiline
-  //                 fullWidth
-  //                 rows="3"
-  //                 placeholder="Amount Notes"
-  //                 value={grantInfo.amount_notes}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.formContainer}>
-  //               <TextField
-  //                 label="Geographic Region"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="geographic_region"
-  //                 placeholder="Geographic Region"
-  //                 value={grantInfo.geographic_region}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //               <TextField
-  //                 label="Domain Areas"
-  //                 type="text"
-  //                 className={styles.inputText}
-  //                 name="domain_areas"
-  //                 placeholder="Domain Areas"
-  //                 value={grantInfo.domain_areas}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-
-  //               <TextField
-  //                 label="Target Demographic"
-  //                 // type="text"
-  //                 className={styles.inputText}
-  //                 name="target_entrepreneur_demographic"
-  //                 placeholder="Target Entrepreneur Demographic"
-  //                 value={grantInfo.target_entrepreneur_demographic}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.formContainer}>
-  //               <TextField
-  //                 label="Notes"
-  //                 // type="text"
-  //                 className={styles.notes}
-  //                 name="notes"
-  //                 multiline
-  //                 fullWidth
-  //                 rows="3"
-  //                 placeholder="Notes"
-  //                 value={grantInfo.notes}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.formContainer}>
-  //               <TextField
-  //                 label="Early Stage Funding"
-  //                 select
-  //                 className={(styles.inputText, styles.dropDown)}
-  //                 name="early_stage_funding"
-  //                 placeholder="Early Stage Funding"
-  //                 value={grantInfo.early_stage_funding}
-  //                 onChange={handleChanges}
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               >
-  //                 {funding.map(option => (
-  //                   <MenuItem key={option.value} value={option.value}>
-  //                     {option.label}
-  //                   </MenuItem>
-  //                 ))}
-  //               </TextField>
-  //               <TextField
-  //                 // label="Application Due Date"
-  //                 type="date"
-  //                 className={styles.inputText}
-  //                 name="most_recent_application_due_date"
-  //                 // placeholder="Application Due Date"
-  //                 value={grantInfo.most_recent_application_due_date}
-  //                 onChange={handleChanges}
-  //                 helperText="Application Due Date"
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //               <TextField
-  //                 type="date"
-  //                 className={styles.inputText}
-  //                 name="details_last_updated"
-  //                 value={grantInfo.details_last_updated}
-  //                 onChange={handleChanges}
-  //                 helperText="Details Last Updated"
-  //                 margin="normal"
-  //                 variant="outlined"
-  //               />
-  //             </div>
-  //             <div className={styles.divButton}>
-  //               <Button
-  //                 type="submit"
-  //                 variant="contained"
-  //                 color="primary"
-  //                 size="large"
-  //                 className={styles.submit}
-  //               >
-  //                 <span className={styles.label}>Submit</span>
-  //               </Button>
-  //             </div>
-  //           </form>
-  //         </div>
-  //       </Grid>
-  //     </Grid>
-  //     {/* </Container> */}
-  //   </div>
-  // );
 };
 const mapStateToProps = ({ grantData, isFetching, error }) => ({
   grantData,
--- grantly/src/components/submitForm/GrantFocus.js
+++ grantly/src/components/submitForm/GrantFocus.js
@@ -44,18 +44,6 @@ const GrantFocus = props => {
             onChange={props.handleChanges}
           />
         </Grid>
-        {/* <Grid item xs={12}>
-          <TextField
-            type="date"
-            name="details_last_updated"
-            // value={props.grantInfo.details_last_updated}
-            // value={Date.now()}
-            onChange={props.handleChanges}
-            helperText="Details Last Updated"
-            margin="normal"
-            variant="outlined"
-          />
-        </Grid> */}
       </Grid>
     </Fragment>
   );
