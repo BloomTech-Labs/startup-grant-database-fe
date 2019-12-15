b9c26915a2eceb03103083fd1fb5895d862cb387
Kristin Barr <kristinbarr@gmail.com>
2019-12-15 14:58:03 -0500

    sitemap footer styling and positioning updates

--- grantly/src/styles/sitemapStyles.js
+++ grantly/src/styles/sitemapStyles.js
@@ -1,21 +1,19 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const sitemapStyles = makeStyles(theme => ({
-  container: {
+  sitemapContainer: {
     // background: "#3A3A3A",
     // color: "#fff",
     // background: "#83D7D1",
     color: "#696969",
     background: "#fff",
-    maxWidth: "100%",
-    padding: "0 2%",
+    width: "100%",
     // position: "fixed",
-    // bottom: "0",
-    fontFamily: "Roboto, sans-serif",
+    position: "absolute",
+    bottom: 0,
+    // padding: "0",
     letterSpacing: "1px",
     zIndex: "3000",
-  
-
     [theme.breakpoints.down("xs")]: {
       background: "#65D8CF",
       color: "#fff",
@@ -23,48 +21,57 @@ export const sitemapStyles = makeStyles(theme => ({
       justifyContent: "center"
     }
   },
-  item: {
-    margin: ".5em 5% 0",
-    [theme.breakpoints.down("xs")]: {
-      margin: "0",
-      padding: "0",
-      width: "100%"
-    }
-  },
   sitemap: {
-    height: "100%",
-    margin: "0 -32px",
+    display: "flex",
+    justifyContent: "space-between",
     alignItems: "center",
-    [theme.breakpoints.down("md")]: {
-      display: "block"
+    height: "100%",
+    color: "#696969",
+    background: "#fff",
+    margin: "0",
+    letterSpacing: "1px",
+    // backgroundColor: "coral",
+    [theme.breakpoints.down("xs")]: {
+      // display: "block",
+      backgroundColor: "#65D8CF",
+      color: "#fff",
+      height: "100%",
+      justifyContent: "center",
+      flexDirection: "column",
     }
   },
-  links: {
-    display: "flex",
-    height: "100%",
-    flexDirection: "column",
-    justifyContent: "space-between",
-
+  item: {
+    margin: "0 auto",
+    padding: "0",
+    // width: "100%",
+    // backgroundColor: "pink",
     [theme.breakpoints.down("xs")]: {
-      height: "150%"
+      // backgroundColor: "pink",
+      margin: "0",
+      // padding: "0",
+      // width: "100%"
     }
   },
   link: {
+    margin: "0",
+    padding: "0",
     color: "#696969",
     "&:hover": {
       cursor: "pointer"
     },
     [theme.breakpoints.down("md")]: {
-      margin: "15px"
+      margin: ".5em 1em"
     },
-
     [theme.breakpoints.down("xs")]: {
       color: "#fff"
     }
   },
   copy: {
+    margin: "0 5% 1em 5%",
+    // backgroundColor: "tan",
     [theme.breakpoints.down("xs")]: {
-      fontSize: ".5rem"
+      fontSize: "1rem",
+      marginBottom: "4em"
     }
   }
 }));
