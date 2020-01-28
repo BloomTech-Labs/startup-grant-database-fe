527d42fe6b1f4b720ce6c50318a735cb0f1cc461
Donald Whitely <dswhitely1@gmail.com>
2020-01-28 13:20:27 -0500

    Added Admin Fetch Grants Success

--- grantly/src/store/grants/grantReducer.ts
+++ grantly/src/store/grants/grantReducer.ts
@@ -8,11 +8,11 @@ const initialState: GrantState = {
 
 export const grantReducer = (state = initialState, action: GrantActions): GrantState => {
     switch (action.type) {
-        case GrantTypes.FETCH_GRANTS_START:
+        case GrantTypes.FETCH_GRANTS_START || GrantTypes.FETCH_ADMIN_GRANTS_START:
             return {...state, grants: [], isLoading: true, errors: null};
-        case GrantTypes.FETCH_GRANTS_SUCCESS:
+        case GrantTypes.FETCH_GRANTS_SUCCESS || GrantTypes.FETCH_ADMIN_GRANTS_SUCCESS:
             return {...state, grants: action.payload, isLoading: false, errors: null};
-        case GrantTypes.FETCH_GRANTS_FAILURE:
+        case GrantTypes.FETCH_GRANTS_FAILURE || GrantTypes.FETCH_ADMIN_GRANTS_FAILURE:
             return {...state, grants: [], isLoading: false, errors: action.payload};
         default:
             return state;
