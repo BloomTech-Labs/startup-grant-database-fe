634088530b05a9076c5ac562fffd9cce743e26fb
Donald Whitely <dswhitely1@gmail.com>
2020-02-01 12:22:31 -0500

    Added Actions to Context

--- grantly/src/components/App.jsx
+++ grantly/src/components/App.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {CssBaseline} from "@material-ui/core";
 import Navbar from "./navbar/Navbar";
 import {Footer} from './footer/Footer';
+import {useActions} from "../store/useActions";
+import {ActionsProvider} from "../context/ActionsContext";
 
 function App() {
+    const actions = useActions();
     return (
-        <>
+        <ActionsProvider value={actions}>
             <CssBaseline/>
             <Navbar/>
             <Footer/>
-        </>
+        </ActionsProvider>
     )
 }
 
