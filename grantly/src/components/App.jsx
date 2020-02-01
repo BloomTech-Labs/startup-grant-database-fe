b7d9748d148745dcb9496f5496706c75e23726e2
Donald Whitely <dswhitely1@gmail.com>
2020-02-01 13:29:04 -0500

    Added Theme Provider To App

--- grantly/src/components/App.jsx
+++ grantly/src/components/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {CssBaseline} from "@material-ui/core";
+import {MuiThemeProvider} from "@material-ui/core/styles";
 import Navbar from "./navbar/Navbar";
 import {Footer} from './footer/Footer';
 import {useActions} from "../store/useActions";
 import {ActionsProvider} from "../context/ActionsContext";
+import {theme} from "./theme";
 
 function App() {
     const actions = useActions();
     return (
-        <ActionsProvider value={actions}>
-            <CssBaseline/>
-            <Navbar/>
-            <Footer/>
-        </ActionsProvider>
+        <MuiThemeProvider theme={theme}>
+            <ActionsProvider value={actions}>
+                <CssBaseline/>
+                <Navbar/>
+                <Footer/>
+            </ActionsProvider>
+        </MuiThemeProvider>
     )
 }
 
