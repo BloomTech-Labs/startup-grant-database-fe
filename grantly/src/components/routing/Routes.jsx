7c2afed1b251ead62bded082204c28f533884a01
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-03 13:05:00 -0600

    implemented suggestion routes

--- grantly/src/components/routing/Routes.jsx
+++ grantly/src/components/routing/Routes.jsx
@@ -1,34 +1,41 @@
-import React from 'react';
+import React from "react";
 import LandingPage from "../landingpage/LandingPage";
 import GrantContainer from "../grants/GrantContainer";
 import RenderRoutes from "./RenderRoutes";
+import { SuggestionForm } from "../suggestion/Suggestion";
 
 const routes = [
-    {
-        key: 'APP_ROOT',
-        path: '/',
+  {
+    key: "APP_ROOT",
+    path: "/",
+    exact: true,
+    component: LandingPage
+  },
+  {
+    key: "GRANTS",
+    path: "/grants",
+    component: RenderRoutes,
+    routes: [
+      {
+        path: "/grants",
         exact: true,
-        component: LandingPage
-    },
-    {
-        key: 'GRANTS',
-        path: '/grants',
-        component: RenderRoutes,
-        routes: [
-            {
-                path: '/grants',
-                exact: true,
-                key: 'GRANTS_ROOT',
-                component: GrantContainer
-            },
-            {
-                path: '/grants/favorites',
-                exact: true,
-                key: 'GRANTS_FAVORITES',
-                component: GrantContainer
-            },
-        ]
-    }
+        key: "GRANTS_ROOT",
+        component: GrantContainer
+      },
+      {
+        path: "/grants/favorites",
+        exact: true,
+        key: "GRANTS_FAVORITES",
+        component: GrantContainer
+      }
+    ]
+  },
+  {
+    key: "SUGGESTION",
+    path: "/suggestion",
+    exact: true,
+    component: SuggestionForm
+  }
 ];
 
-export default routes
+export default routes;
