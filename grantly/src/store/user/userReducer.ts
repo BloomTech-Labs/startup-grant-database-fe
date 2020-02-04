b9aa8599dc00abe64a18853a4a17fcc2d5687b42
Donald Whitely <dswhitely1@gmail.com>
2020-02-04 12:16:32 -0500

    Refactoring Redux Store for Clean

--- grantly/src/store/user/userReducer.ts
+++ grantly/src/store/user/userReducer.ts
@@ -1,4 +1,5 @@
 import {UserActions, UserState, UserTypes} from "./userTypes";
+import {createReducer} from "../utils/createReducer";
 
 const initialState: UserState = {
     currentUser: {
@@ -17,29 +18,24 @@ const initialState: UserState = {
     errors: null,
 };
 
-export const userReducer = (state = initialState, action: UserActions): UserState => {
-    switch (action.type) {
-        case UserTypes.FETCH_USER_START:
-            return {...state, isLoading: true};
-        case UserTypes.FETCH_USER_SUCCESS:
-            return {...state, isLoading: false, errors: null, currentUser: action.payload};
-        case UserTypes.FETCH_USER_FAILURE:
-            return {...state, isLoading: false, errors: action.payload};
-        case UserTypes.SET_USER_FROM_AUTH0:
-            return {...state, isLoading: false, errors: null, currentUser: {...action.payload}};
-        case UserTypes.IS_MODERATOR:
-            return {...state, isModerator: true};
-        case UserTypes.SET_TOKEN:
-            return {...state, token: action.payload};
-        case UserTypes.FETCH_FAVORITES_START:
-            return {...state, isLoading: true};
-        case UserTypes.FETCH_FAVORITES_SUCCESS:
-            return {...state, favoriteGrants: action.payload, isLoading: false};
-        case UserTypes.FETCH_FAVORITES_FAILURE:
-            return {...state, isLoading: false, errors: action.payload};
-        case UserTypes.RESET_USER:
-            return initialState;
-        default:
-            return state;
-    }
-};
+type FunctionReducer<S extends UserState = UserState, P = any> = (state:UserState, payload?: any) => UserState;
+
+const userStartReducer: FunctionReducer = (state) => ({...state, isLoading: true});
+const userSetAuth0Reducer: FunctionReducer = (state, payload) => ({...state, isLoading: false, errors: null, currentUser: {...payload}});
+const userFailureReducer: FunctionReducer = (state, payload) => ({...state, isLoading: false, errors: payload});
+const userFetchFavoritesSuccess: FunctionReducer = (state, payload) => ({...state, isLoading: false, favoriteGrants: payload});
+const userIsModeratorReducer: FunctionReducer = state => ({...state, isModerator: true});
+const userSetTokenReducer: FunctionReducer = (state, payload) => ({...state, token: payload});
+const userResetUserReducer: FunctionReducer = () => initialState;
+
+export const userReducer = createReducer(initialState, {
+    [UserTypes.FETCH_FAVORITES_START]: userStartReducer,
+    [UserTypes.FETCH_FAVORITES_SUCCESS]: userFetchFavoritesSuccess,
+    [UserTypes.FETCH_FAVORITES_FAILURE]: userFailureReducer,
+    [UserTypes.SET_USER_FROM_AUTH0]: userSetAuth0Reducer,
+    [UserTypes.IS_MODERATOR]: userIsModeratorReducer,
+    [UserTypes.SET_TOKEN]: userSetTokenReducer,
+    [UserTypes.RESET_USER]: userResetUserReducer,
+    [UserTypes.POST_FAVORITES_START]: userStartReducer
+});
+
