61ff70bb59f3ab6e3ab4cdf330f183b71be33801
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-05 16:46:44 -0600

    added to the reusable field

--- grantly/src/components/suggestion/formElements/TextFormField.jsx
+++ grantly/src/components/suggestion/formElements/TextFormField.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, MenuItem } from "@material-ui/core";
 
 export const TextFormField = props => {
+  // console.log("the data prop => ", props.data);
+  console.log("props", props);
   return (
     <Grid item xs={12}>
       <TextField
         fullWidth
+        select={props.select}
         multiline={props.multiline}
         rows={props.rows}
         variant={props.variant}
@@ -15,7 +18,13 @@ export const TextFormField = props => {
         placeholder={props.label}
         value={null}
         onChange={null}
-      />
+      >
+        {props.data.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
     </Grid>
   );
 };
