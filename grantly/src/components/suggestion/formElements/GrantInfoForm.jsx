5ec9ba442dab3ef19f58286a8e74bbe51d127449
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-05 16:47:02 -0600

    refactored info form

--- grantly/src/components/suggestion/formElements/GrantInfoForm.jsx
+++ grantly/src/components/suggestion/formElements/GrantInfoForm.jsx
@@ -4,31 +4,13 @@ import { connect } from "react-redux";
 import formStyles from "../formElements/formStyles";
 import { Grid, Typography, TextField, Divider } from "@material-ui/core";
 import { TextFormField } from "../formElements/TextFormField.jsx";
+import { InfoFieldData } from "../formUtils/formValues.js";
 
 const useStyles = makeStyles(theme => ({
   bottomBox: {
     padding: theme.spacing(2, 6, 1, 6)
   }
 }));
-
-const infoFieldData = [
-  {
-    label: "Grant Name",
-    type: "text",
-    name: "competition_name",
-    multiline: false
-  },
-  { label: "Website", type: "url", name: "website", multiline: false },
-  { label: "Amount", type: "number", name: "amount", multiline: false },
-  {
-    label: "Amount Notes",
-    type: "text",
-    name: "amount_notes",
-    rows: "3",
-    variant: "outlined",
-    multiline: true
-  }
-];
 
 export const GrantInfoForm = props => {
   const styles = useStyles();
@@ -39,7 +21,7 @@ export const GrantInfoForm = props => {
       </Typography>
       <Divider variant="middle" />
       <Grid container spacing={3} className={styles.bottomBox}>
-        {infoFieldData.map(data => (
+        {InfoFieldData.map(data => (
           <TextFormField
             label={data.label}
             type={data.type}
@@ -47,6 +29,7 @@ export const GrantInfoForm = props => {
             rows={data.rows}
             variant={data.variant}
             multiline={data.multiline}
+            data={data.data}
           />
         ))}
       </Grid>
