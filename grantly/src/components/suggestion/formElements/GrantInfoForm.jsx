0154cfa8d108e16a7003a7e7e63b907499275b7a
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-05 14:42:35 -0600

    refactored grant info form

--- grantly/src/components/suggestion/formElements/GrantInfoForm.jsx
+++ grantly/src/components/suggestion/formElements/GrantInfoForm.jsx
@@ -3,12 +3,32 @@ import { makeStyles } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 import formStyles from "../formElements/formStyles";
 import { Grid, Typography, TextField, Divider } from "@material-ui/core";
+import { TextFormField } from "../formElements/TextFormField.jsx";
 
 const useStyles = makeStyles(theme => ({
   bottomBox: {
     padding: theme.spacing(2, 6, 1, 6)
   }
 }));
+
+const infoFieldData = [
+  {
+    label: "Grant Name",
+    type: "text",
+    name: "competition_name",
+    multiline: false
+  },
+  { label: "Website", type: "url", name: "website", multiline: false },
+  { label: "Amount", type: "number", name: "amount", multiline: false },
+  {
+    label: "Amount Notes",
+    type: "text",
+    name: "amount_notes",
+    rows: "3",
+    variant: "outlined",
+    multiline: true
+  }
+];
 
 export const GrantInfoForm = props => {
   const styles = useStyles();
@@ -19,53 +39,16 @@ export const GrantInfoForm = props => {
       </Typography>
       <Divider variant="middle" />
       <Grid container spacing={3} className={styles.bottomBox}>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Grant Name"
-            type="text"
-            name="competition_name"
-            placeholder="Grant Name"
-            value={null}
-            onChange={null}
+        {infoFieldData.map(data => (
+          <TextFormField
+            label={data.label}
+            type={data.type}
+            name={data.name}
+            rows={data.rows}
+            variant={data.variant}
+            multiline={data.multiline}
           />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Website"
-            type="url"
-            fullWidth
-            name="website"
-            placeholder="Website"
-            value={null}
-            onChange={null}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Amount"
-            type="number"
-            name="amount"
-            fullWidth
-            placeholder="Amount"
-            value={null}
-            onChange={null}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Amount Notes"
-            type="text"
-            name="amount_notes"
-            multiline
-            fullWidth
-            rows="3"
-            placeholder="Amount Notes"
-            value={null}
-            onChange={null}
-            variant="outlined"
-          />
-        </Grid>
+        ))}
       </Grid>
     </Fragment>
   );
