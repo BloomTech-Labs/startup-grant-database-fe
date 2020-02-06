cdcfab00372daa73b9aff4301399472950dd7d4b
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-06 10:59:02 -0600

    edit working

--- grantly/src/components/admin/GrantTable.jsx
+++ grantly/src/components/admin/GrantTable.jsx
@@ -80,45 +80,45 @@ const GrantTable = props => {
           columns={tableValues.columns}
           options={tableStyles}
           data={grants}
-          // editable={{
-          //   onRowAdd: newData =>
-          //     new Promise(resolve => {
-          //       setTimeout(() => {
-          //         resolve();
-          //         let filteredData = Object.assign({}, newData);
-          //         delete filteredData.requests;
-          //         props.postGrants(filteredData, props.currentUser.token);
-          //       }, 600);
-          //     }),
-          //   onRowUpdate: (newData, oldData) =>
-          //     new Promise(resolve => {
-          //       setTimeout(() => {
-          //         resolve();
-          //         if (oldData) {
-          //           let filteredData = Object.assign({}, newData);
-          //           delete filteredData.requests;
-          //           props.putGrants(
-          //             {
-          //               ...filteredData,
-          //               details_last_updated: moment().format("YYYY-MM-DD")
-          //             },
-          //             props.currentUser.token
-          //           );
-          //         }
-          //       }, 600);
-          //     }),
-          //   onRowDelete: oldData =>
-          //     new Promise(resolve => {
-          //       setTimeout(() => {
-          //         resolve();
-          //         if (oldData) {
-          //           delete oldData.requests;
-          //           props.deleteGrants(oldData.id, props.currentUser.token);
-          //         }
-          //       }, 600);
-          //     })
-          // }}
-          // zeroMinWidth
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  let filteredData = Object.assign({}, newData);
+                  delete filteredData.requests;
+                  props.postGrants(filteredData, props.currentUser.token);
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    let filteredData = Object.assign({}, newData);
+                    delete filteredData.requests;
+                    props.putGrants(
+                      {
+                        ...filteredData,
+                        details_last_updated: moment().format("YYYY-MM-DD")
+                      },
+                      props.currentUser.token
+                    );
+                  }
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    delete oldData.requests;
+                    props.deleteGrants(oldData.id, props.currentUser.token);
+                  }
+                }, 600);
+              })
+          }}
+          zeroMinWidth
         />
       </Paper>
     </React.Fragment>
