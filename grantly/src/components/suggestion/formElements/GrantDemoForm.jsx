6a9ff60780d586d0bdcacce26c5dcf7c40f43b1a
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-05 22:13:57 -0600

    completed refactor of demo form

--- grantly/src/components/suggestion/formElements/GrantDemoForm.jsx
+++ grantly/src/components/suggestion/formElements/GrantDemoForm.jsx
@@ -9,13 +9,9 @@ import {
   MenuItem,
   Divider
 } from "@material-ui/core";
-import {
-  geographicRegion,
-  targetDemographic,
-  funding
-} from "../formUtils/formValues.js";
+import { DemoFormData } from "../formUtils/formValues.js";
 import formStyles from "../formElements/formStyles";
-
+import { TextFormField } from "../formElements/TextFormField.jsx";
 const useStyles = makeStyles(theme => ({
   bottomBox: {
     padding: theme.spacing(2, 6, 1, 6)
@@ -31,77 +27,22 @@ export const GrantDemoForm = props => {
       <Typography variant="h5">Grant Demographics</Typography>
       <Divider variant="middle" />
       <Grid container spacing={3} className={styles.bottomBox}>
-        <Grid item xs={12}>
-          <TextField
-            label="Geographic Region"
-            select
-            fullWidth
-            name="geographic_region"
-            placeholder="Geographic Region"
-          >
-            {/* Maps through the array to return values for dropdown */}
-            {geographicRegion.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Target Demographic"
-            select
-            fullWidth
-            name="target_entrepreneur_demographic"
-            placeholder="Target Entrepreneur Demographic"
-          >
-            {/* Maps through the array to return values for dropdown */}
-
-            {targetDemographic.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label="Application Due Date"
-            InputLabelProps={{ shrink: true }}
-            fullWidth
-            type="date"
-            name="most_recent_application_due_date"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label="Early Stage Funding"
-            select
-            fullWidth
-            name="early_stage_funding"
-            placeholder="Early Stage Funding"
-          >
-            {/* Maps through the array to return values for dropdown */}
-            {funding.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Description"
-            type="text"
-            name="notes"
-            multiline
-            fullWidth
-            rows="3"
-            placeholder="Please provide a description of the grant here!"
-            margin="normal"
-            variant="outlined"
-          />
-        </Grid>
+        {DemoFormData.map(data => {
+          return (
+            <TextFormField
+              label={data.label}
+              type={data.type}
+              name={data.name}
+              select={data.select}
+              data={data.data}
+              sm={props.sm}
+              inputLabel={data.inputLabel}
+              multiline={data.multiline}
+              variant={data.variant}
+              rows={data.rows}
+            />
+          );
+        })}
       </Grid>
     </Fragment>
   );
