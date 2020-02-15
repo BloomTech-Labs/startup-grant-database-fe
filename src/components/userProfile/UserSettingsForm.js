4599b0546b1a07484d75f9ec5e7aae47e2014277
Donald Whitely <dswhitely1@gmail.com>
2020-02-14 20:59:03 -0500

    User Routes are now working with Data from the Auth0

--- src/components/userProfile/UserSettingsForm.js
+++ src/components/userProfile/UserSettingsForm.js
@@ -1,84 +1,73 @@
-import React, { useContext } from "react";
-import { TextFormField } from "../suggestion/formElements/TextFormField";
-import { Divider, Grid, Paper, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { ActionsContext } from "../../context/ActionsContext";
-import { useAuth0 } from "../auth0/Auth0Wrapper";
-import { useSelector } from "react-redux";
-import { useForm } from "../../hooks/useForm";
-import { logger } from "../../store/utils/logger";
+import React from "react";
+import {TextFormField} from "../suggestion/formElements/TextFormField";
+import {Divider, Grid, Paper, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  formContainer: {
-    padding: "1em 8em 2em 8em",
-    [theme.breakpoints.down("sm")]: {
-      padding: "2em"
+    formContainer: {
+        padding: "1em 8em 2em 8em",
+        [theme.breakpoints.down("sm")]: {
+            padding: "2em"
+        }
+    },
+    layout: {
+        [theme.breakpoints.down("sm")]: {
+            padding: "4em 0"
+        }
+    },
+    title: {
+        paddingTop: "1.5em"
     }
-  },
-  layout: {
-    [theme.breakpoints.down("sm")]: {
-      padding: "4em 0"
-    }
-  },
-  title: {
-    paddingTop: "1.5em"
-  }
 }));
 
 const userSettingsFormData = [
-  { label: "First Name", type: "text", name: "first_name", data: [] },
-  { label: "Last Name", type: "text", name: "last_name", data: [] },
-  { label: "Role", type: "text", name: "role", data: [] },
-  { label: "Phone", type: "tel", name: "phone", data: [] },
-  { label: "Project", type: "text", name: "company", data: [] },
-  { label: "Project Website", type: "text", name: "company_url", data: [] },
-  {
-    label: "About Project",
-    type: "text",
-    name: "about",
-    multiline: true,
-    variant: "outlined",
-    rows: "3",
-    // styles: styles.about,
-    data: []
-  }
+    {label: "First Name", type: "text", name: "first_name", data: []},
+    {label: "Last Name", type: "text", name: "last_name", data: []},
+    {label: "Role", type: "text", name: "role", data: []},
+    {label: "Phone", type: "tel", name: "phone", data: []},
+    {label: "Project", type: "text", name: "company", data: []},
+    {label: "Project Website", type: "text", name: "company_url", data: []},
+    {
+        label: "About Project",
+        type: "text",
+        name: "about",
+        multiline: true,
+        variant: "outlined",
+        rows: "3",
+        // styles: styles.about,
+        data: []
+    }
 ];
 
 export const UserSettingsForm = props => {
-  logger("props", props);
-  const styles = useStyles();
-  const actions = useContext(ActionsContext);
+    const styles = useStyles();
 
-  const { token, currentUser, pgUser } = useSelector(state => state.user);
-  const { isAuthenticated } = useAuth0();
-
-  return (
-    <Paper className={styles.layout}>
-      <Typography variant="h6" className={styles.title}>
-        Personal Details
-      </Typography>
-      <Divider variant="middle" />
-      <form onSubmit={props.handleSubmit}>
-        <Grid container spacing={3} className={styles.formContainer}>
-          {userSettingsFormData.map(data => {
-            logger("value of TextFormField", props.values[data.name]);
-            return (
-              <TextFormField
-                label={data.label}
-                type={data.type}
-                name={data.name}
-                multiline={data.multiline}
-                rows={data.rows}
-                variant={data.variant}
-                sm={6}
-                data={data.data}
-                handleChanges={props.handleChange}
-                values={props.values}
-              />
-            );
-          })}
-        </Grid>
-      </form>
-    </Paper>
-  );
+    return (
+        <Paper className={styles.layout}>
+            <Typography variant="h6" className={styles.title}>
+                Personal Details
+            </Typography>
+            <Divider variant="middle"/>
+            <form onSubmit={props.handleSubmit}>
+                <Grid container spacing={3} className={styles.formContainer}>
+                    {userSettingsFormData.map(data => {
+                        return (
+                            <TextFormField
+                                label={data.label}
+                                type={data.type}
+                                name={data.name}
+                                multiline={data.multiline}
+                                rows={data.rows}
+                                variant={data.variant}
+                                sm={6}
+                                data={data.data}
+                                handleChanges={props.handleChange}
+                                values={props.values}
+                            />
+                        );
+                    })}
+                </Grid>
+            </form>
+        </Paper>
+    );
 };
--- src/components/userProfile/userSettings.js
+++ src/components/userProfile/userSettings.js
@@ -1,91 +1,79 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useSelector } from "react-redux";
-import { UserSettingsForm } from "./UserSettingsForm.js";
-import { UserData } from "./userData.js";
-import { Container, Button } from "@material-ui/core";
-import { ActionsContext } from "../../context/ActionsContext";
-import { makeStyles } from "@material-ui/core/styles";
-import { AuthForm } from "./AuthForm.js";
-import { useForm } from "../../hooks/useForm";
+import React, {useContext, useState} from "react";
+import {useSelector} from "react-redux";
+import {UserSettingsForm} from "./UserSettingsForm.js";
+import {UserData} from "./userData.js";
+import {Button, Container} from "@material-ui/core";
+import {ActionsContext} from "../../context/ActionsContext";
+import {makeStyles} from "@material-ui/core/styles";
+import {useForm} from "../../hooks/useForm";
 
 const useStyles = makeStyles(theme => ({
-  button: {
-    margin: "2em"
-  }
+    button: {
+        margin: "2em"
+    }
 }));
 
 const UserSettings = () => {
-  const actions = useContext(ActionsContext);
-  const [isEditing, setIsEditing] = useState(false);
-  const { token, currentUser, pgUser } = useSelector(state => state.user);
-  const styles = useStyles();
-  const [values, handleChange, handleSubmit] = useForm(
-    {
-      first_name: "",
-      last_name: "",
-      role: "",
-      phone: "",
-      company: "",
-      company_url: "",
-      about: ""
-    },
-    doSubmit
-  );
+    const actions = useContext(ActionsContext);
+    const [isEditing, setIsEditing] = useState(false);
+    const {token, currentUser} = useSelector(state => state.user);
+    const styles = useStyles();
+    const [values, handleChange, handleSubmit] = useForm(currentUser.user_metadata, doSubmit);
 
-  function doSubmit() {
-    actions.user.updateUser(token, values);
-  }
+    function doSubmit() {
+        actions.user.updateUser(token, values);
+    }
 
-  return (
-    <React.Fragment>
-      <Container maxWidth="lg">
-        {/* <AuthForm /> */}
-        {/* <UserSettingsForm /> */}
-        {!isEditing ? (
-          <UserData />
-        ) : (
-          <UserSettingsForm
-            values={values}
-            handleChange={handleChange}
-            handleSubmit={handleSubmit}
-          />
-        )}
-        {!isEditing && (
-          <Button
-            variant="contained"
-            color="primary"
-            className={styles.button}
-            onClick={() => !isEditing && setIsEditing(true)}
-          >
-            Edit Details
-          </Button>
-        )}
-        {isEditing && (
-          <>
-            <Button
-              variant="contained"
-              color="primary"
-              className={styles.button}
-              onClick={e => {
-                handleSubmit(e);
-                setIsEditing(false);
-              }}
-            >
-              Save Changes
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              className={styles.button}
-              onClick={() => isEditing && setIsEditing(false)}
-            >
-              Cancel Edit
-            </Button>
-          </>
-        )}
-      </Container>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Container maxWidth="lg">
+                {/* <AuthForm /> */}
+                {/* <UserSettingsForm /> */}
+                {!isEditing ? (
+                    <UserData/>
+                ) : (
+                    <UserSettingsForm
+                        values={values}
+                        handleChange={handleChange}
+                        handleSubmit={handleSubmit}
+                    />
+                )}
+                {!isEditing && (
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className={styles.button}
+                        onClick={() => !isEditing && setIsEditing(true)}
+                    >
+                        Edit Details
+                    </Button>
+                )}
+                {isEditing && (
+                    <>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={styles.button}
+                            onClick={e => {
+                                handleSubmit(e);
+                                setIsEditing(false);
+                            }}
+                        >
+                            Save Changes
+                        </Button>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            className={styles.button}
+                            onClick={() => isEditing && setIsEditing(false)}
+                        >
+                            Cancel Edit
+                        </Button>
+                    </>
+                )}
+            </Container>
+        </React.Fragment>
+    );
 };
 
 export default UserSettings;
