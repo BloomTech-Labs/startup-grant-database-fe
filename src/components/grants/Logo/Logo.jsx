0b090c89a0656c6d95d1ffff17f8a9e081c98b2c
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-19 17:23:00 -0600

    logo rendering from api. needs dynamicness to each company

--- src/components/grants/Logo/Logo.jsx
+++ src/components/grants/Logo/Logo.jsx
@@ -1,8 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { ReactComponent as DefaultLogo } from "./defaultLogo.svg";
+import axios from "axios";
 
 export const Logo = props => {
-  useEffect(() => {}, []);
+  let domain = "fuelmade.com";
+  const [imageUrl, setImageUrl] = useState();
+  useEffect(() => {
+    axios
+      .get(`https://logo.clearbit.com/${domain}?size=75`)
+      .then(res => {
+        setImageUrl(res.config.url);
+      })
+      .catch(err => {
+        console.log("logo not returned", err);
+      });
+  }, [domain]);
 
-  return <DefaultLogo />;
+  console.log("the image url", imageUrl);
+
+  return <img src={imageUrl} alt="logo" />;
 };
