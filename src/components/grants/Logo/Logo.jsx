b3e697140508c7492ea28cc38005d4eeb5bf70e9
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-19 20:00:51 -0600

    Logo component done

--- src/components/grants/Logo/Logo.jsx
+++ src/components/grants/Logo/Logo.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { ReactComponent as DefaultLogo } from "./defaultLogo.svg";
+import { ReactComponent as DefaultLogo } from "./defaultGrantLogo.svg";
 import axios from "axios";
 
+const modifiedUrl = oldUrl => {
+  const loc = new URL(oldUrl);
+  return loc.hostname;
+};
+
 export const Logo = props => {
-  let domain = "fuelmade.com";
   const [imageUrl, setImageUrl] = useState();
+
   useEffect(() => {
     axios
-      .get(`https://logo.clearbit.com/${domain}?size=75`)
+      .get(`https://logo.clearbit.com/${modifiedUrl(props.url)}?size=75`)
       .then(res => {
+        console.log("res.config.url", res.config.url);
         setImageUrl(res.config.url);
       })
       .catch(err => {
         console.log("logo not returned", err);
+        setImageUrl("");
       });
-  }, [domain]);
+  }, []);
 
-  console.log("the image url", imageUrl);
-
-  return <img src={imageUrl} alt="logo" />;
+  return imageUrl === "" ? <DefaultLogo /> : <img src={imageUrl} alt="logo" />;
 };
