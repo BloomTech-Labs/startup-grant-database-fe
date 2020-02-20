846202237116f2e5ccfeb48f8c5d7088475a8c8f
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-19 20:16:48 -0600

    Logo added to showcase

--- src/components/grants/showcase/ShowcaseTopContent.jsx
+++ src/components/grants/showcase/ShowcaseTopContent.jsx
@@ -1,69 +1,75 @@
-import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import {Grid, Link, Typography} from '@material-ui/core';
-import {useSelector} from "react-redux";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Link, Typography } from "@material-ui/core";
+import { useSelector } from "react-redux";
 import FavoritesIconButton from "./FavoritesIconButton";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import BookmarkBorderOutlinedIcon from "@material-ui/icons/BookmarkBorderOutlined";
 import DeleteIcon from "@material-ui/icons/Delete";
 import LanguageIcon from "@material-ui/icons/Language";
 import IconDisplay from "./IconDisplay";
+import { Logo } from "../Logo/Logo.jsx";
 
 const useStyles = makeStyles(theme => ({
-    title: {
-        maxWidth: '300px'
-    },
-    topContent: {
-        margin: "0"
-    },
-    showcaseHeader: {
-        width: 'inherit'
-    },
-    grantName: {
-        fontWeight: 700,
-        fontFamily: 'Nunito Sans',
-        color: '#222222',
-        marginBottom: theme.spacing(2),
-        padding: theme.spacing(2,0,0,0),
-    },
-    website: {
-        fill: '#696969',
-        marginRight: ".2rem",
-        fontSize: "1.2rem",
-        overflow: "none",
-    }
+  title: {
+    maxWidth: "300px"
+  },
+  topContent: {
+    margin: "0"
+  },
+  showcaseHeader: {
+    width: "inherit"
+  },
+  grantName: {
+    fontWeight: 700,
+    fontFamily: "Nunito Sans",
+    color: "#222222",
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(2, 0, 0, 0)
+  },
+  website: {
+    fill: "#696969",
+    marginRight: ".2rem",
+    fontSize: "1.2rem",
+    overflow: "none"
+  }
 }));
 
-function ShowcaseTopContent({showcase}) {
-    const {favoriteGrants} = useSelector(state => state.user);
-    const existingFavorite = favoriteGrants.filter(fav => fav.id === showcase.id);
-    const classes = useStyles();
+function ShowcaseTopContent({ showcase }) {
+  const { favoriteGrants } = useSelector(state => state.user);
+  const existingFavorite = favoriteGrants.filter(fav => fav.id === showcase.id);
+  const classes = useStyles();
 
-    return (
-        <>
-            <Grid container justify="space-between" alignItems='center'>
-                <Grid item className={classes.title}>
-                <Typography variant='h5' className={classes.grantName}>
-                    {showcase.competition_name}
-                </Typography>
-                </Grid>
-                <Grid item>
-                   <IconDisplay favoriteGrants={favoriteGrants} id={showcase.id} existingFavorite={existingFavorite} />
-                </Grid>
-            </Grid>
-            <Grid
-                container
-                justify='flex-start'
-                alignItems='flex-end'
-                alignContent='flex-end'
-            >
-                <LanguageIcon className={classes.website} />
-                <Link href={showcase.website} target="_blank">
-                    {showcase.website}
-                </Link>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid container justify="space-between" alignItems="center">
+        <Logo url={showcase.website} />
+        <Grid item className={classes.title}>
+          <Typography variant="h5" className={classes.grantName}>
+            {showcase.competition_name}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <IconDisplay
+            favoriteGrants={favoriteGrants}
+            id={showcase.id}
+            existingFavorite={existingFavorite}
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        justify="flex-start"
+        alignItems="flex-end"
+        alignContent="flex-end"
+      >
+        <LanguageIcon className={classes.website} />
+        <Link href={showcase.website} target="_blank">
+          {showcase.website}
+        </Link>
+      </Grid>
+    </>
+  );
 }
 
 export default ShowcaseTopContent;
