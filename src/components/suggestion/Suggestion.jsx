c25f511067c0085083fef32a9e783688a458aee3
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-22 14:47:27 -0600

    my current  mess of test rip

--- src/components/suggestion/Suggestion.jsx
+++ src/components/suggestion/Suggestion.jsx
@@ -1,7 +1,4 @@
 import React, { Component, useState } from "react";
-import GrantDemoForm from "./formElements/GrantDemoForm";
-import GrantFocusForm from "./formElements/GrantDemoForm";
-import GrantInfoForm from "./formElements/GrantDemoForm";
 import { AddGrant } from "./SuggestionForm";
 
 export const SuggestionForm = props => {
