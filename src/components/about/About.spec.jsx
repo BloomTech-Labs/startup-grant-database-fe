4a3c74870fc39c95d96c682245dd0c309b7a084a
Donald Whitely <dswhitely1@gmail.com>
2020-02-22 15:40:20 -0500

    About Page Tested

---
+++ src/components/about/About.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import About from './About';
+
+describe('About.jsx', () => {
+    it('renders without crashing', () => {
+        const wrapper = render(<About />);
+        const lambda = wrapper.getAllByText(/lambda/i);
+        expect(lambda).toBeInTheDocument
+    })
+});
