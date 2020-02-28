a72442f03f59ce7074458bb7029779922bfc805f
Jackson McComas <jackson.mccomas@gmail.com>
2020-02-28 16:00:37 -0600

    Created mail list component

--- src/components/admin/EmailFormSingle.jsx
+++ src/components/admin/EmailFormSingle.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 import { TextFormField } from "../suggestion/formElements/TextFormField";
 import { Grid, makeStyles, Paper, Button, Typography } from "@material-ui/core";
 import { EmailSingleValues } from "./values/EmailFormValues";
+import { useForm } from "../../hooks/useForm";
+import { ActionsContext } from "../../context/ActionsContext";
+import ConfirmedModel from "./ConfirmedModel";
 
 const emailFormStyles = makeStyles(theme => ({
   paper: {
@@ -17,35 +21,68 @@ const emailFormStyles = makeStyles(theme => ({
 }));
 
 export const EmailFormSingle = () => {
+  const actions = useContext(ActionsContext);
+  const { token } = useSelector(state => state.user);
+  const { isSuccess } = useSelector(state => state.admin);
   const styles = emailFormStyles();
-  return (
-    <form className={styles.form}>
-      <Paper className={styles.paper}>
-        <Typography variant="h3">Email To Single User</Typography>
-        <Grid container className={styles.formInputs}>
-          {EmailSingleValues.map(data => {
-            return (
-              <TextFormField
-                label={data.label}
-                type={data.type}
-                name={data.name}
-                select={data.select}
-                data={data.data}
-                //   sm={props.sm}
-                multiline={data.multiline}
-                variant={data.variant}
-                rows={data.rows}
-                value={data.name}
-                //   handleChanges={props.handleChanges}
-              />
-            );
-          })}
-        </Grid>
+  const [values, handleChange, handleSubmit, resetForm] = useForm(
+    {
+      to: "",
+      subject: "",
+      text: ""
+    },
+    doSubmit
+  );
 
-        <Button variant="contained" color="secondary" className={styles.button}>
-          Send To Single User
-        </Button>
-      </Paper>
-    </form>
+  const handleClose = () => {
+    actions.admin.resetSuccess();
+    resetForm();
+  };
+
+  function doSubmit() {
+    actions.admin.postEmailSingleText(token, values);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <Paper className={styles.paper}>
+          <Typography variant="h3">Email To Single User</Typography>
+          <Grid container className={styles.formInputs}>
+            {EmailSingleValues.map(data => {
+              return (
+                <TextFormField
+                  label={data.label}
+                  type={data.type}
+                  name={data.name}
+                  select={data.select}
+                  data={data.data}
+                  //   sm={props.sm}
+                  multiline={data.multiline}
+                  variant={data.variant}
+                  rows={data.rows}
+                  value={data.name}
+                  handleChanges={handleChange}
+                />
+              );
+            })}
+          </Grid>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            className={styles.button}
+            type="submit"
+          >
+            Send To Single User
+          </Button>
+        </Paper>
+      </form>
+      <ConfirmedModel
+        isSuccess={isSuccess}
+        handleClose={handleClose}
+        title="Success!"
+        message="The specified user have been sent the email"
+      />
+    </>
   );
 };
